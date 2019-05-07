5774857f4c9647bc88c801acef3ed8c22f6a179f
jojeringnax <hollow718@gmail.com>
2019-05-07 17:16:06 +0200

    initial commit #2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -95,5 +93,9 @@
                 </div>
             </div>
         </div>
+        <div id="example"></div>
+
+
+        <script src="{{asset('/js/app.js')}}"></script>
     </body>
 </html>
